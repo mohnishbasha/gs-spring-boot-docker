11d99ec373f748702e91cd1b0c0fffdce0c2d70f
Mohinish <mohnishbasha@users.noreply.github.com>
2016-03-02 00:48:34 -0800

    Adding datadog integration

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -5,13 +5,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.StatsDClient;
 
 @SpringBootApplication
 @RestController
 public class Application {
+	
+	private static final StatsDClient statsd = new NonBlockingStatsDClient(
+            "springboot.home",                          /* prefix to any stats; may be null or empty string */
+            "localhost",                        /* common case: localhost */
+            8125,                                 /* port */
+            new String[] {"tag:role:springboot"}            /* DataDog extension: Constant tags, always applied */
+        );
 
 	@RequestMapping("/")
 	public String home() {
+		statsd.incrementCounter("pageview");
+                statsd.recordGaugeValue("pageviewguage", 100);
+                statsd.recordGaugeValue("pageviewfloat", 0.01); /* DataDog extension: support for floating-point gauges */
+                statsd.recordHistogramValue("pageviewhist", 15);     /* DataDog extension: histograms */
+                statsd.recordHistogramValue("pageviewhist", 15.5);   /* ...also floating-point */
+
+                /* Compatibility note: Unlike upstream statsd, DataDog expects execution times to be a
+                * floating-point value in seconds, not a millisecond value. This library
+                * does the conversion from ms to fractional seconds.
+                */
+                statsd.recordExecutionTime("pageviewclustertag", 25, "cluster:foo"); /* DataDog extension: cluster tag */
 		return "Hello Docker World";
 	}
 
